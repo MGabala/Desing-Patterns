f217383136d1178e1e334eb911df71a92133673a
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-04-24 14:19:13 +0200

    Add project files.

---
+++ Creational/Singleton/Implementation.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazylogger = new Lazy<Logger>(() => new Logger());
+        //private static Logger? _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazylogger.Value;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+            }
+        }
+        protected Logger()
+        {
+
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Orders
+{
+    public int Price { get; set; } 
+    public string Name { get; set; }
+
+    public Orders(int price, string name)
+    {
+        Price = price;   
+        Name = name;
+    }
+    List<Orders> orders = new List<Orders>();
+    
+}
