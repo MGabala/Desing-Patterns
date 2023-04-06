d9c97197c88b5249ca0b76fe0b656d4119f1da0f
Mateusz <82496006+MGabala@users.noreply.github.com>
2023-04-06 17:44:33 +0200

    Proxy

--- Program.cs
+++ Program.cs
@@ -153,11 +153,46 @@
 //Console.WriteLine($"Size of topLevelDirectory is: {topLevelDirectory.GetSize()}");
 //#endregion
 
-#region Facade
-using Facade;
-Console.Title = "Facade";
-var facade = new DiscountFacade();
+//#region Facade
+//using Facade;
+//Console.Title = "Facade";
+//var facade = new DiscountFacade();
 
-Console.WriteLine($"Discount percentage for customer with id 1: {facade.CalculateDiscountPercentage(1)}");
-Console.WriteLine($"Discount percentage for customer with id 10: {facade.CalculateDiscountPercentage(10 )}");
+//Console.WriteLine($"Discount percentage for customer with id 1: {facade.CalculateDiscountPercentage(1)}");
+//Console.WriteLine($"Discount percentage for customer with id 10: {facade.CalculateDiscountPercentage(10 )}");
+//#endregion
+
+#region Proxy
+using Proxy;
+Console.Title = "Proxy";
+
+// without proxy
+Console.WriteLine("Constructing document.");
+var myDocument = new Proxy.Document("MyDocument.pdf");
+Console.WriteLine("Document constructed.");
+myDocument.DisplayDocument();
+
+Console.WriteLine();
+
+// with proxy 
+Console.WriteLine("Constructing document proxy.");
+var myDocumentProxy = new Proxy.DocumentProxy("MyDocument.pdf");
+Console.WriteLine("Document proxy constructed.");
+myDocumentProxy.DisplayDocument();
+
+Console.WriteLine();
+
+// with chained proxy
+Console.WriteLine("Constructing protected document proxy.");
+var myProtectedDocumentProxy = new Proxy.ProtectedDocumentProxy("MyDocument.pdf", "Viewer");
+Console.WriteLine("Protected document proxy constructed.");
+myProtectedDocumentProxy.DisplayDocument();
+
+Console.WriteLine();
+
+// with chained proxy, no access
+Console.WriteLine("Constructing protected document proxy.");
+myProtectedDocumentProxy = new Proxy.ProtectedDocumentProxy("MyDocument.pdf", "AnotherRole");
+Console.WriteLine("Protected document proxy constructed.");
+myProtectedDocumentProxy.DisplayDocument();
 #endregion
