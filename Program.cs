64c6b8b55156e1eb5e0772afef53871a25112d92
Mateusz <82496006+MGabala@users.noreply.github.com>
2023-04-13 16:03:58 +0200

    Memento

--- Program.cs
+++ Program.cs
@@ -255,9 +255,41 @@
 #endregion
 
 #region Command
-using Command;
+//using Command;
 
-Console.Title = "Command";
+//Console.Title = "Command";
+
+//CommandManager commandManager = new();
+//IEmployeeManagerRepository repository = new EmployeeManagerRepository();
+
+//commandManager.Invoke(
+//    new AddEmployeeToManagerList(repository, 1, new Employee(111, "Kevin")));
+//repository.WriteDataStore();
+
+//commandManager.Undo();
+//repository.WriteDataStore();
+
+//commandManager.Invoke(
+//    new AddEmployeeToManagerList(repository, 1, new Employee(222, "Clara")));
+//repository.WriteDataStore();
+
+//commandManager.Invoke(
+//    new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+//repository.WriteDataStore();
+
+//// try adding the same employee again
+//commandManager.Invoke(
+//    new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+//repository.WriteDataStore();
+
+//commandManager.UndoAll();
+//repository.WriteDataStore();
+#endregion
+
+#region Memento
+using Memento;
+
+Console.Title = "Memento";
 
 CommandManager commandManager = new();
 IEmployeeManagerRepository repository = new EmployeeManagerRepository();
