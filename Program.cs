928a9d9d7f256d3f9651c1db9717100b71364582
Mateusz <82496006+MGabala@users.noreply.github.com>
2023-05-13 20:31:00 +0200

    State

--- Program.cs
+++ Program.cs
@@ -376,26 +376,47 @@
 #endregion
 
 #region Observer
-using Observer;
+//using Observer;
 
-Console.Title = "Observer";
+//Console.Title = "Observer";
 
-TicketStockService ticketStockService = new();
-TicketResellerService ticketResellerService = new();
-OrderService orderService = new();
+//TicketStockService ticketStockService = new();
+//TicketResellerService ticketResellerService = new();
+//OrderService orderService = new();
 
-// add two observers
-orderService.AddObserver(ticketStockService);
-orderService.AddObserver(ticketResellerService);
+//// add two observers
+//orderService.AddObserver(ticketStockService);
+//orderService.AddObserver(ticketResellerService);
 
-// notify
-orderService.CompleteTicketSale(1, 2);
+//// notify
+//orderService.CompleteTicketSale(1, 2);
 
-Console.WriteLine();
+//Console.WriteLine();
 
-// remove one observer
-orderService.RemoveObserver(ticketResellerService);
+//// remove one observer
+//orderService.RemoveObserver(ticketResellerService);
 
-// notify
-orderService.CompleteTicketSale(2, 4);
+//// notify
+//orderService.CompleteTicketSale(2, 4);
+#endregion
+
+#region State
+using State;
+
+Console.Title = "State";
+
+BankAccount bankAccount = new();
+bankAccount.Deposit(100);
+bankAccount.Withdraw(500);
+bankAccount.Withdraw(100);
+// deposit enough to go to gold
+bankAccount.Deposit(2000);
+// should be in gold now
+bankAccount.Deposit(100);
+// back to overdraw
+bankAccount.Withdraw(3000);
+// should transition to regular
+bankAccount.Deposit(3000);
+// should still be in regular
+bankAccount.Deposit(100);
 #endregion
