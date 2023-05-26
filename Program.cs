c4f61c581e1ed7451c0da53131f424c1800072d9
Mateusz <82496006+MGabala@users.noreply.github.com>
2023-05-26 19:27:34 +0200

    Iterator

--- Program.cs
+++ Program.cs
@@ -401,22 +401,53 @@
 #endregion
 
 #region State
-using State;
+//using State;
 
-Console.Title = "State";
+//Console.Title = "State";
 
-BankAccount bankAccount = new();
-bankAccount.Deposit(100);
-bankAccount.Withdraw(500);
-bankAccount.Withdraw(100);
-// deposit enough to go to gold
-bankAccount.Deposit(2000);
-// should be in gold now
-bankAccount.Deposit(100);
-// back to overdraw
-bankAccount.Withdraw(3000);
-// should transition to regular
-bankAccount.Deposit(3000);
-// should still be in regular
-bankAccount.Deposit(100);
+//BankAccount bankAccount = new();
+//bankAccount.Deposit(100);
+//bankAccount.Withdraw(500);
+//bankAccount.Withdraw(100);
+//// deposit enough to go to gold
+//bankAccount.Deposit(2000);
+//// should be in gold now
+//bankAccount.Deposit(100);
+//// back to overdraw
+//bankAccount.Withdraw(3000);
+//// should transition to regular
+//bankAccount.Deposit(3000);
+//// should still be in regular
+//bankAccount.Deposit(100);
+#endregion
+
+#region Iterator
+using Iterator;
+
+using Prototype;
+
+Console.Title = "Iterator";
+
+// create the collection
+PeopleCollection people = new();
+
+people.Add(new Iterator.Person("Mateusz Gabała", "Polska"));
+people.Add(new Iterator.Person("Second Person", "Belgium"));
+people.Add(new Iterator.Person("Third Person", "The Netherlands"));
+people.Add(new Iterator.Person("Fourth Person", "Germany"));
+
+// create the iterator
+var peopleIterator = people.CreateIterator();
+
+// use the iterator to run through the people
+// in the collection; they should come out 
+// in alphabetical order
+for (Iterator.Person person = peopleIterator.First();
+    !peopleIterator.IsDone;
+    person = peopleIterator.Next())
+{
+    Console.WriteLine(person?.Name);
+}
+
+Console.ReadKey();
 #endregion
